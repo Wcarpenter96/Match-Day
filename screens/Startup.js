cf1593fd0375005e5b37d1d7b65e3f37714c0a8a
Weston Carpenter <wc.cc96@gmail.com>
2020-01-25 12:23:31 -0800

    initial app file structure

---
+++ screens/Startup.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Startup = props => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default Startup;
